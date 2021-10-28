dfd2b9901d3478a2085c699af26088300d19dee9
unknown <joao31004@fiec.edu.br>
2021-10-28 13:52:57 -0300

    commit1

---
+++ ambiente.js
@@ -0,0 +1,14 @@
+var c = 2
+while (c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}
+
+/*mesma coisa disso em verde só q da maneira certa
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+*/
---
+++ ambiente_alternativo.js
@@ -0,0 +1,14 @@
+var c = 2
+do{
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*mesma coisa disso em verde só q da maneira certa
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+console.log(`passo ${c}`)
+*/
